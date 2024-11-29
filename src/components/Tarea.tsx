2e3d259e0783ca2aea5a03b95b9831ebfc626497
JediOut <jedi.viera@gmail.com>
2024-11-29 15:49:26 -0600

    Proyecto de lista de tareas basico

---
+++ src/components/Tarea.tsx
@@ -0,0 +1,15 @@
+
+
+type TareaProps = {
+    tarea:string
+    borrarTarea:()=> void
+}
+
+export const Tarea = ({tarea, borrarTarea}: TareaProps) => {
+  return (
+    <div className="task">
+        <span>{tarea}</span>
+        <button onClick={borrarTarea}>Eliminar</button>
+    </div>
+  )
+}
---
+++ src/components/TodoApp.tsx
@@ -0,0 +1,32 @@
+import { useState } from "react";
+import { ListaTareas } from "./ListaTareas";
+
+const TodoApp = () => {
+  const [nuevaTarea, setNuevaTarea] = useState<string>("");
+  const [listaTareas, setlistaTareas] = useState<string[]>([]);
+  const handleAddTask = () => {
+    if (nuevaTarea.trim() == "") return;
+    setlistaTareas((tareasAnteriores) => [...tareasAnteriores, nuevaTarea]);
+    setNuevaTarea("");
+  };
+  const handleBorrarTarea = (index: number) => {
+    setlistaTareas((tareas) => tareas.filter((_, i) => i !== index));
+  };
+  return (
+    <div>
+      <h1>Lista de tareas</h1>
+      <div>
+        <input
+          type="text"
+          value={nuevaTarea}
+          onChange={(e) => setNuevaTarea(e.target.value)}
+          placeholder="Nueva Tarea"
+        />
+        <button className="" onClick={handleAddTask}>Agregar Tarea</button>
+      </div>
+      <ListaTareas listaTareas={listaTareas} borrarTarea={handleBorrarTarea} />
+    </div>
+  );
+};
+
+export default TodoApp;
